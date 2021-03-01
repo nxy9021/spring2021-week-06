4213ccdf02f01b99c0762c4c1f8edc3bcd7d9812
Nanxi Yu <nanxiyu@gmail.com>
2021-03-01 17:01:14 -0600

    Added class notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,56 @@
-console.log('Hello world');
+// //Numbers
+// console.log(2+10);
+
+// // - bigint
+// console.log(1000000000000000n);
+
+// // - decimals
+// console.log(1.1+23.1+23);
+// console.log(1.1 + 2.2);
+
+// //booleans
+// console.log(true);
+// console.log(false);
+
+// //undefined: not set
+// console.log(undefined);
+
+// //null: empty
+// // console.log(null);
+
+
+// //how to test type
+
+// //objects
+// console.log(typeof null);
+// console.log(typeof True);
+
+// //arrays
+// //functions
+
+// // let x =3
+
+
+// temperate literal
+let fname = 'Nanxi';
+let lname = 'Yu';
+let fullname = `my name is ${Nanxi} ${Yu}`;
+console.log(fullname);
+
+
+//arrays - grocery list
+// () parenthesis
+// [] bracket
+// {} braces
+
+// remove from the end: pop
+// remove from the start: shift
+// add from the end: push
+
+// convert array to set
+// array counts everything
+// set counts unique value
+// dictionary is like array
+// property in dictionary can have a space when it has a ''
+// use [] in the dictionary to allow the space
+// or use the dot . which does not allow any space
